07a69c15512ed9a962c07b24710bb59f0edbec6b
Lokesh Soni <verrmaji001@gmail.com>
2025-05-12 08:34:00 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,17 @@ hands.setOptions({
   minTrackingConfidence: 0.5
 });
 
+const videoElement = document.querySelector('video');
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await hands.send({ image: videoElement });
+  },
+  width: 640,
+  height: 480
+});
+camera.start();
+
 
 export default {
 	darkMode: ["class"],
